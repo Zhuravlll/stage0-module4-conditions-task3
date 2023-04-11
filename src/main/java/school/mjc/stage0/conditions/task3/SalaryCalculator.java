8d52e049797a7213fd2ed4cafa0972dacf8a4d33
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-04-11 17:53:17 +0300

    Module #4.1, task #3 completed tasks using multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double afterTax;
+        if (salary < 10000 && salary > 0) {
+            System.out.println(afterTax = salary * 0.85);
+        }
+        if (salary > 10000 && salary < 20000) {
+            System.out.println(afterTax = salary * 0.82);
+        }
+        if (salary > 20000) {
+            System.out.println(afterTax = salary * 0.80);
+        }
+        if (salary <= 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
